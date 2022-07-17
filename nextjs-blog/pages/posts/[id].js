b93ce6eec16d03850388a647ea859264efabbeac
ManupaDev <manupadevsuite@gmail.com>
2022-07-17 23:40:47 +0530

    added scaffolding

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,12 +1,3 @@
-# Dynamic Routes
-
-![](2022-07-17-23-32-05.png)
-
-- we’ll talk about the case where each page path depends on external data. Next.js allows you to statically generate pages with paths that depend on external data. This enables dynamic URLs in Next.js.
-
-![](2022-07-17-23-35-35.png)
-
-```jsx
 import Layout from "../../components/layout";
 
 export default function Post() {
@@ -20,5 +11,3 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }) {
   // Fetch necessary data for the blog post using params.id
 }
-
-```
